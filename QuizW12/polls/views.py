0027b864d03ce840dbc25494024a235e21fda54b
i2x <0x8084@gmail.com>
2025-02-13 18:38:01 +0700

    copy and pate from old project

---
+++ QuizW12/polls/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Question, Choice
+
+def index(request):
+    questions = Question.objects.all()
+    return render(request, 'polls/index.html', {'questions': questions})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice.votes += 1
+        selected_choice.save()
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    return redirect('polls:results', question_id=question.id)
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
